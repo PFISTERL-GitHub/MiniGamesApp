8a616f85a72a566b140c726476431136635fdadf
PFISTER Ludovic <ludovicp2005@gmail.com>
2026-05-05 12:01:19 +0200

    adding expected structure

--- app/src/main/java/com/pfisterludovicmiehealix/minigames/ui/home/HomeScreen.kt
+++ app/src/main/java/com/pfisterludovicmiehealix/minigames/ui/home/HomeScreen.kt
@@ -1,2 +1,6 @@
 package com.pfisterludovicmiehealix.minigames.ui.home
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen ( onPlayClick : () -> Unit ) { }
--- app/src/main/java/com/pfisterludovicmiehealix/minigames/ui/reaction/ReactionScreen.kt
+++ app/src/main/java/com/pfisterludovicmiehealix/minigames/ui/reaction/ReactionScreen.kt
@@ -1,2 +1,6 @@
 package com.pfisterludovicmiehealix.minigames.ui.reaction
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ReactionScreen ( onBackClick : () -> Unit ) { }
