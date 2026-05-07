d3d0da9326219fd72d366c57486e9a9ed8b1cc93
PFISTER Ludovic <ludovicp2005@gmail.com>
2026-05-07 16:20:40 +0200

    Etape 2 MainActivity avec navigation

--- app/src/main/java/com/pfisterludovicmiehealix/minigames/MainActivity.kt
+++ app/src/main/java/com/pfisterludovicmiehealix/minigames/MainActivity.kt
@@ -3,49 +3,34 @@ package com.pfisterludovicmiehealix.minigames
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.*
+import com.pfisterludovicmiehealix.minigames.ui.home.HomeScreen
+import com.pfisterludovicmiehealix.minigames.ui.reaction.ReactionScreen
 import com.pfisterludovicmiehealix.minigames.ui.theme.MiniGamesAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MiniGamesAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MiniGamesApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MiniGamesApp() {
+    var isPlayingReaction by remember { mutableStateOf(false) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MiniGamesAppTheme {
-        Greeting("Android")
+    if (isPlayingReaction) {
+        ReactionScreen(
+            onBackClick = { isPlayingReaction = false }
+        )
+    } else {
+        HomeScreen(
+            onPlayClick = { isPlayingReaction = true }
+        )
     }
 }
 
-// MainActivity .kt
-@Composable
-fun MiniGamesApp () { }
