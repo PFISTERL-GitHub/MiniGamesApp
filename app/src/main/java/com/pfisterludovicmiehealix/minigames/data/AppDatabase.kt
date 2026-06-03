9eae25b705c534a12c64b2fb9e1664a25a1f6e2a
MIEHE Alix <alixmiehe2004@gmail.com>
2026-06-03 14:27:45 +0200

    added appdatabase implementation

--- app/src/main/java/com/pfisterludovicmiehealix/minigames/data/AppDatabase.kt
+++ app/src/main/java/com/pfisterludovicmiehealix/minigames/data/AppDatabase.kt
@@ -1,4 +1,27 @@
 package com.pfisterludovicmiehealix.minigames.data
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Score::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun scoreDao(): ScoreDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "minigames_database"
+                ).build().also { INSTANCE = it }
+            }
+        }
+    }
 }
