1ca68980b82c516c6014215303013eff39036d18
MIEHE Alix <alixmiehe2004@gmail.com>
2026-06-03 14:30:31 +0200

    added score repository implementation

--- app/src/main/java/com/pfisterludovicmiehealix/minigames/data/ScoreRepository.kt
+++ app/src/main/java/com/pfisterludovicmiehealix/minigames/data/ScoreRepository.kt
@@ -1,2 +1,6 @@
 package com.pfisterludovicmiehealix.minigames.data
 
+class ScoreRepository(private val dao: ScoreDao) {
+    suspend fun insertScore(score: Score) = dao.insertScore(score)
+    suspend fun getTopScores(): List<Score> = dao.getTopScores()
+}
