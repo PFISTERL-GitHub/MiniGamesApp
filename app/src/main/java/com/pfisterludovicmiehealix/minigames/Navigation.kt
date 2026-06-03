90406383b2599ed5e8715ba5d1bbcdb76bace5ca
MIEHE Alix <alixmiehe2004@gmail.com>
2026-06-03 14:38:26 +0200

    implemented naviguation changes to Navigation - new route and player name

--- app/src/main/java/com/pfisterludovicmiehealix/minigames/Navigation.kt
+++ app/src/main/java/com/pfisterludovicmiehealix/minigames/Navigation.kt
@@ -6,43 +6,36 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.pfisterludovicmiehealix.minigames.ui.home.HomeScreen
+import com.pfisterludovicmiehealix.minigames.ui.leaderboard.LeaderboardScreen
 import com.pfisterludovicmiehealix.minigames.ui.reaction.ReactionScreen
 import com.pfisterludovicmiehealix.minigames.ui.wordgame.WordGameScreen
 
-// ROUTES
-
 object Routes {
-    const val HOME     = "home"
-    const val REACTION = "reaction"
-    const val WORD     = "word"
+    const val HOME        = "home"
+    const val REACTION    = "reaction"
+    const val WORD        = "word"
+    const val LEADERBOARD = "leaderboard"
 }
 
-// NAVIGATION
-
 @Composable
-fun MiniGamesNavHost(
-    navController: NavHostController = rememberNavController()
-) {
-    NavHost(
-        navController    = navController,
-        startDestination = Routes.HOME
-    ) {
+fun MiniGamesNavHost(navController: NavHostController = rememberNavController()) {
+    NavHost(navController = navController, startDestination = Routes.HOME) {
         composable(Routes.HOME) {
             HomeScreen(
-                onPlayClick = { route -> navController.navigate(route) }
+                onPlayClick = { route, playerName -> navController.navigate("$route/$playerName") },
+                onLeaderboardClick = { navController.navigate(Routes.LEADERBOARD) }
             )
         }
-
-        composable(Routes.REACTION) {
-            ReactionScreen(
-                onBackClick = { navController.popBackStack() }
-            )
+        composable("${Routes.REACTION}/{playerName}") { backStackEntry ->
+            val playerName = backStackEntry.arguments?.getString("playerName") ?: ""
+            ReactionScreen(playerName = playerName, onBackClick = { navController.popBackStack() })
         }
-
-        composable(Routes.WORD) {
-            WordGameScreen(
-                onBackClick = { navController.popBackStack() }
-            )
+        composable("${Routes.WORD}/{playerName}") { backStackEntry ->
+            val playerName = backStackEntry.arguments?.getString("playerName") ?: ""
+            WordGameScreen(playerName = playerName, onBackClick = { navController.popBackStack() })
+        }
+        composable(Routes.LEADERBOARD) {
+            LeaderboardScreen(onBackClick = { navController.popBackStack() })
         }
     }
 }
